d87d88073edfeb719082822ffab2ad9677f2937c
Kim Kyeongho <sgn07124@gmail.com>
2025-07-05 22:30:21 +0900

    initial commit

---
+++ src/main/java/project/CsArena/CsArenaApplication.java
@@ -0,0 +1,13 @@
+package project.CsArena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/CsArena/CsArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package project.CsArena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
